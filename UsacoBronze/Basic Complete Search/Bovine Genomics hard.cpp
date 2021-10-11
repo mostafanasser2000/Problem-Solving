30d9a010ff6c828ae501d91ddf4964e62fd7f01a
Mostafa Nasser Mohamed Ali <mustafanasser503@gmail.com>
2021-10-11 14:57:28 +0200

    Update Bovine Genomics hard.cpp

--- UsacoBronze/Basic Complete Search/Bovine Genomics hard.cpp
+++ UsacoBronze/Basic Complete Search/Bovine Genomics hard.cpp
@@ -45,42 +45,7 @@ bool comp(ii a, ii b) {
     return a.S > b.S;
 }
 
-void combinationUtil(int arr[], int n, int r,
-                     int index, int data[], int i);
 
-
-void printCombination(int arr[], int n, int r)
-{
-
-
-    int data[r];
-
-    combinationUtil(arr, n, r, 0, data, 0);
-}
-
-
-void combinationUtil(int arr[], int n, int r, int index,
-                     int data[], int i)
-{
-
-    if (index == r) {
-        for (int j = 0; j < r; j++)
-            cout <<" "<< data[j];
-        cout <<"\n";
-        return;
-    }
-
-
-    if (i >= n)
-        return;
-
-
-    data[index] = arr[i];
-    combinationUtil(arr, n, r, index + 1, data, i + 1);
-
-
-    combinationUtil(arr, n, r, index, data, i + 1);
-}
 int main(){
     IOS
     freopen("input.txt","r",stdin);
